47fce7c7f73f7225c3deaddd592ed3df4ff31d67
Karina Shakir <karinashakir@mail.ru>
2024-08-05 18:07:02 +0700

    entry point

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log("Hello, Hexlet!");
+import _ from "lodash";
+console.log(_.last(["one", "two"]));
+const myFunction = () => {
+};
+export { myFunction };
+import { myFunction } from './src/index.js';
+
+myFunction();
